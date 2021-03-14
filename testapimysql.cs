2d3d6dd1f9bf7aabd74ce5c914052baa5a41ecfa
Shubham Sudam Panchal <79783665+SHUBHAM10081998@users.noreply.github.com>
2021-03-14 12:50:32 +0530

    Update testapimysql.cs

--- testapimysql.cs
+++ testapimysql.cs
@@ -70,28 +70,3 @@ namespace TESTAPI.Controllers
     }
 }
 
-
-
-
-
-
-model
-
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace TESTAPI.Models
-{
-    public class TestModel
-    {
-        public string name { get; set; }
-        public string sname { get; set; }
-    }
-    public class response
-    {
-        public string MESSAGE_CODE { get; set; }
-    }
-}
