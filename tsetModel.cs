11fa404f25cde9e63cb44f1050db47e07ee2bbb1
Shubham Sudam Panchal <79783665+SHUBHAM10081998@users.noreply.github.com>
2021-03-14 12:51:03 +0530

    Create tsetModel.cs

---
+++ tsetModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TESTAPI.Models
+{
+    public class TestModel
+    {
+        public string name { get; set; }
+        public string sname { get; set; }
+    }
+    public class response
+    {
+        public string MESSAGE_CODE { get; set; }
+    }
+}
